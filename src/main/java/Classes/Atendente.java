8a838ceb6830db23b14d0a655b58b90b43a92513
Leonardo <leonardogarciasou@gmail.com>
2021-05-31 20:55:30 -0300

    teste

--- src/main/java/Classes/Atendente.java
+++ src/main/java/Classes/Atendente.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Classes;
 
-/**
- *
- * @author leona
- */
 public class Atendente {
-    
+    public void consultarFila(){
+        
+    }
+    public void confirmarVacinacao(){
+        
+    }
 }
