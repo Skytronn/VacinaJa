ff8e13b9f92b19cdc0c399c9425e7e0173e645d7
JhonasRocha <joaovt.rochar@gmail.com>
2021-06-14 18:23:18 -0300

    imagens

--- src/main/java/Telas/TelaFilaVacinacao.java
+++ src/main/java/Telas/TelaFilaVacinacao.java
@@ -114,7 +114,7 @@ public class TelaFilaVacinacao extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addGap(287, 287, 287)
                         .addComponent(jLabel2)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGap(26, 26, 26)
                         .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(98, Short.MAX_VALUE))
         );
